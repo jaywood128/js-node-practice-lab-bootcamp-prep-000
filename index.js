79f3ba7c17b85a5d7a2de458bc2e466fcedf635b
Johnathon Wood <jaywood128@gmail.com>
2018-09-26 19:57:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var height = 72
 
 console.log("Name:")
 console.log(name)
-
+for (i = 0;i < 10;i++) {
+  console.log("Height:")
+  console.log("height + 1")
+}
 console.log("Height:")
 console.log(height + 1)
+}
